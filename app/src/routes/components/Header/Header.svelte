ad1043482d7e74ef073b078598b070a8ba5133ed
Max <bomax1597@gmail.com>
2024-04-14 21:45:39 +0200

    header update

--- app/src/routes/components/Header/Header.svelte
+++ app/src/routes/components/Header/Header.svelte
@@ -7,18 +7,21 @@
 <header>
     <div class="header_container">
         <div class="header_logo">
-            <a href="/">
-                <img src={logo} alt="logo" />
-            </a>
+
+            <p><span>ELTA</span><br>MUSIC</p>
+
         </div>
         <div class="header_links">
-            <a href="/">Home</a>
-            <a href="/about">About</a>
+            <p>products</p>
+            <p>shop</p>
+            <p>retailers</p>
+            <p>discontinued</p>
+            <p>About</p>
         </div>
-        <div class="header_github">
-            <a href="https://github.com/michal-josef-spacek">
-                <img src={github} alt="github" />
-            </a>
+        <div class="header_right">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+            </svg>
         </div>
 </header>
 
