7e5d33b264ae5a0a60dd07bffb2f6286d03acfdc
MaxB0zz <bomax1597@gmail.com>
2024-06-28 16:55:34 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	app/src/routes/+page.svelte
    #	app/src/routes/_page.sass

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import ThreeDViewer from "./components/3DViewer/ThreeDViewer.svelte";
 
 	let show_teapot = false
+	let yb_links = ["https://www.youtube.com/embed/MifoWuu6a98", "https://www.youtube.com/embed/T4MhAel0PoM"]
 	function switchModel() {
 		show_teapot = !show_teapot;
 	}
@@ -26,6 +27,8 @@
 			}
 		};
 	}
+
+	let clicked_vid_1, clicked_vid_2 = false
 </script>
 
 <svelte:head>
@@ -36,8 +39,19 @@
 <section >
 	<div class="page" >
 		<section class="header_pvk">
-			<h1><span class="colored_header_pvk">PF1</span>. Polyvoks</h1>
-			<p>A double-stereo filter.</p>
+			<div>
+				<h1><span class="colored_header_pvk">PF1</span>. Polyvoks</h1>
+				<p>A double-stereo filter.</p>
+			</div>
+			<div class="dl_content">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-pdf-fill" viewBox="0 0 16 16">
+					<path d="M5.523 12.424q.21-.124.459-.238a8 8 0 0 1-.45.606c-.28.337-.498.516-.635.572l-.035.012a.3.3 0 0 1-.026-.044c-.056-.11-.054-.216.04-.36.106-.165.319-.354.647-.548m2.455-1.647q-.178.037-.356.078a21 21 0 0 0 .5-1.05 12 12 0 0 0 .51.858q-.326.048-.654.114m2.525.939a4 4 0 0 1-.435-.41q.344.007.612.054c.317.057.466.147.518.209a.1.1 0 0 1 .026.064.44.44 0 0 1-.06.2.3.3 0 0 1-.094.124.1.1 0 0 1-.069.015c-.09-.003-.258-.066-.498-.256M8.278 6.97c-.04.244-.108.524-.2.829a5 5 0 0 1-.089-.346c-.076-.353-.087-.63-.046-.822.038-.177.11-.248.196-.283a.5.5 0 0 1 .145-.04c.013.03.028.092.032.198q.008.183-.038.465z"/>
+					<path fill-rule="evenodd" d="M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2m5.5 1.5v2a1 1 0 0 0 1 1h2zM4.165 13.668c.09.18.23.343.438.419.207.075.412.04.58-.03.318-.13.635-.436.926-.786.333-.401.683-.927 1.021-1.51a11.7 11.7 0 0 1 1.997-.406c.3.383.61.713.91.95.28.22.603.403.934.417a.86.86 0 0 0 .51-.138c.155-.101.27-.247.354-.416.09-.181.145-.37.138-.563a.84.84 0 0 0-.2-.518c-.226-.27-.596-.4-.96-.465a5.8 5.8 0 0 0-1.335-.05 11 11 0 0 1-.98-1.686c.25-.66.437-1.284.52-1.794.036-.218.055-.426.048-.614a1.24 1.24 0 0 0-.127-.538.7.7 0 0 0-.477-.365c-.202-.043-.41 0-.601.077-.377.15-.576.47-.651.823-.073.34-.04.736.046 1.136.088.406.238.848.43 1.295a20 20 0 0 1-1.062 2.227 7.7 7.7 0 0 0-1.482.645c-.37.22-.699.48-.897.787-.21.326-.275.714-.08 1.103"/>
+				</svg>
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-paperclip" viewBox="0 0 16 16">
+					<path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0z"/>
+				</svg>
+			</div>
 		</section>
 		<section class="presentation_video" use:fadeOnScrollDown>
 			<div class="dark-filter"></div>
@@ -165,6 +179,49 @@
 					</div>
 
 				</div>
+			</div>
+		</section>
+		<section class="reviewSection" use:fadeOnScrollDown>
+			<div class="YTvids" id="mini_carousel">
+				<div class="vid" id="vid_1">
+					{#if clicked_vid_1}
+						<iframe
+								src={yb_links[0]}
+								title="YouTube video player"
+								allow="accelerometer; autoplay; encrypted-media; gyroscope; gyroscope; picture-in-picture; web-share"
+								allowfullscreen
+						></iframe>
+					{:else}
+						<img src="https://img.youtube.com/vi/MifoWuu6a98/maxresdefault.jpg" alt="YouTube Thumbnail" class="thumbnail" on:click={()=>{ clicked_vid_1 = true}}>
+						<div class="floating_loader_text" on:click={()=>{ clicked_vid_1 = true}}>
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cursor" viewBox="0 0 16 16" >
+								<path d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103zM2.25 8.184l3.897 1.67a.5.5 0 0 1 .262.263l1.67 3.897L12.743 3.52z"/>
+							</svg>
+						</div>
+					{/if}
+				</div>
+				<div class="vid" id="vid_2">
+					{#if clicked_vid_2}
+						<iframe
+								src={yb_links[1]}
+								title="YouTube video player"
+								allow="accelerometer; autoplay; encrypted-media; gyroscope; gyroscope; picture-in-picture; web-share"
+								allowfullscreen
+						></iframe>
+					{:else}
+						<img src="https://img.youtube.com/vi/T4MhAel0PoM/maxresdefault.jpg" alt="YouTube Thumbnail" class="thumbnail" on:click={()=>{ clicked_vid_2 = true}}>
+						<div class="floating_loader_text" on:click={()=>{ clicked_vid_2 = true}}>
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cursor" viewBox="0 0 16 16" >
+								<path d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103zM2.25 8.184l3.897 1.67a.5.5 0 0 1 .262.263l1.67 3.897L12.743 3.52z"/>
+							</svg>
+						</div>
+					{/if}
+
+
+				</div>
+			</div>
+			<div>
+				<p>Check out those Awesome Videos for more Details !</p>
 			</div>
 		</section>
 	</div>
